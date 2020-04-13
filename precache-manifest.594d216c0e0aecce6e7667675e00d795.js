4b36a36014ff8c4576c278acba930ea222989568
trox115 <tofernandes67@gmail.com>
2020-04-13 23:56:09 +0100

    Updates

---
+++ precache-manifest.594d216c0e0aecce6e7667675e00d795.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6595e1f2877529020573716943753608",
+    "url": "/terrincha/index.html"
+  },
+  {
+    "revision": "80d1b08e90992750cbc9",
+    "url": "/terrincha/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ec618f14c244dbfea821",
+    "url": "/terrincha/static/css/main.fa4d86b2.chunk.css"
+  },
+  {
+    "revision": "80d1b08e90992750cbc9",
+    "url": "/terrincha/static/js/2.40579bbd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/terrincha/static/js/2.40579bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec618f14c244dbfea821",
+    "url": "/terrincha/static/js/main.b4113e46.chunk.js"
+  },
+  {
+    "revision": "5ccb5c2771888cf2c3ca",
+    "url": "/terrincha/static/js/runtime-main.092bf354.js"
+  },
+  {
+    "revision": "37fd4fd6aaec9bd6299057eb2b35333f",
+    "url": "/terrincha/static/media/1.37fd4fd6.jpg"
+  },
+  {
+    "revision": "5492e4c386156fb51068617c0781e2f2",
+    "url": "/terrincha/static/media/3.5492e4c3.jpg"
+  },
+  {
+    "revision": "9cfc632d2a19ee2d442b73bced0cf7f9",
+    "url": "/terrincha/static/media/4.9cfc632d.jpg"
+  },
+  {
+    "revision": "f78dd6a197ec8776bff851191c34b494",
+    "url": "/terrincha/static/media/atividades.f78dd6a1.svg"
+  },
+  {
+    "revision": "95b500c737a2b1dcff48bd2d3c4c6e3a",
+    "url": "/terrincha/static/media/comprar.95b500c7.svg"
+  },
+  {
+    "revision": "6d6f972ef62668c3c88d13b0e4c6bbf7",
+    "url": "/terrincha/static/media/info.6d6f972e.svg"
+  },
+  {
+    "revision": "6946eac80ac92aecede1d2807cc2dad6",
+    "url": "/terrincha/static/media/logo.6946eac8.png"
+  },
+  {
+    "revision": "5f31d343c4515e1d67494f89f9abe97f",
+    "url": "/terrincha/static/media/menus.5f31d343.svg"
+  },
+  {
+    "revision": "131cc42aa09a7a08dbf67d6332fe6f5a",
+    "url": "/terrincha/static/media/navegacao.131cc42a.svg"
+  },
+  {
+    "revision": "5770fb8ec6e93e0445b2e64acda70eb5",
+    "url": "/terrincha/static/media/qrcode.5770fb8e.jpg"
+  },
+  {
+    "revision": "d42e2c3d22e5db5903f089a51417abf7",
+    "url": "/terrincha/static/media/wifi.d42e2c3d.svg"
+  }
+]);
