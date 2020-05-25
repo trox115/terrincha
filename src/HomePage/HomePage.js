028ffbee2d77e1f8f3f8d4a95769ea2957e7de97
trox115 <tofernandes67@gmail.com>
2020-05-25 12:37:58 +0100

    new changes sugested by designer

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -5,6 +5,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import {Cartao,Header} from '../style.js';
 import Navegacao from '../assets/img/1.png';
+import EntregaCasa from '../assets/img/2.png';
+import Atividades from '../assets/img/3.png';
+import Encomendar from '../assets/img/5.png';
+import Social from '../assets/img/4.png';
+import Pequeno from '../assets/img/6.png';
+import Wifi from '../assets/img/7.png';
+import Contactos from '../assets/img/8.png';
 
 
 function HomePage() {
@@ -22,7 +29,55 @@ function HomePage() {
 
          </Cartao>
         </Col>
-       
+       <Col md="6">
+         <Cartao>
+          <h1>Escolha e será entregue em sua casa</h1>
+          <img src={EntregaCasa} alt="icone-navegacao" />
+
+         </Cartao>
+        </Col>
+        <Col md="6">
+         <Cartao>
+          <h1>Atividades na Quinta</h1>
+          <img src={Atividades} alt="icone-navegacao" />
+
+         </Cartao>
+        </Col>
+        <Col md="6">
+         <Cartao>
+          <h1>Encomende e leve para casa</h1>
+          <img src={Encomendar} alt="icone-navegacao" />
+
+         </Cartao>
+        </Col>
+        <Col md="6">
+         <Cartao>
+          <h1>Terrincha nas redes sociais</h1>
+          <img src={Social} alt="icone-navegacao" />
+
+         </Cartao>
+        </Col>
+        <Col md="6">
+         <Cartao>
+          <h1>Horas de Pequeno Almoço</h1>
+          <img src={Pequeno} alt="icone-navegacao" />
+
+         </Cartao>
+        </Col>
+        <Col md="6">
+         <Cartao>
+          <h1>Pasword Wifi</h1>
+          <img src={Wifi} alt="icone-navegacao" />
+
+         </Cartao>
+        </Col>
+        <Col md="6">
+         <Cartao>
+          <h1>Contactos úteis</h1>
+          <img src={Contactos} alt="icone-navegacao" />
+
+         </Cartao>
+        </Col>
       </Row>
     </Container>
   );
--- src/style.js
+++ src/style.js
@@ -1,17 +1,19 @@
 import style from 'styled-components';
 
 export const Cartao = style.div`
-width:90%;
+width:100%;
 display:flex;
 align-items:center;
 background-color:white;
 height:150px;
 border-radius:30px;
-
+margin-bottom:5%;
   h1{
-    font-size:30px;
+    font-size:20px;
     width:60%;
     margin-left:2%;
+    font-family: 'Open Sans', sans-serif;
+    font-weight:700;
   }
 
   img{
@@ -24,7 +26,7 @@ border-radius:30px;
 export const Header = style.div`
 width:100%;
 height:200px;
-background-image: url('./terrincha-head.jpg');
+background-image: url('./terrincha-head.jpeg');
 background-size: cover;
 background-position: center;
 border-radius:30px;
