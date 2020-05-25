787fe55bc895e69e3cccbed666dc739123a2b6ec
trox115 <tofernandes67@gmail.com>
2020-05-25 18:31:54 +0100

    finish wifipage

--- src/Wifi/wifi.js
+++ src/Wifi/wifi.js
@@ -4,11 +4,29 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import styled from 'styled-components';
+import { InfoHeader, Cartao, GiveMargin } from '../style';
+import wifi from '../assets/img/7.png';
 import qrcode from '../qrcode.jpg';
 
 const Hidding = styled.div`
+  text-align: center;
+  width: 100%;
   img {
     display: none;
+    width: 350px;
+    height: 350px;
+    margin-bottom: 30px;
+  }
+
+  button {
+    width: 100%;
+    background: #f3a6bc;
+    border-color: #f09898;
+    &:hover {
+      background: #f09898;
+      color: black;
+      border-color: #f3a6bc;
+    }
   }
 `;
 
@@ -16,7 +34,7 @@ function Wifi() {
   function handleClick() {
     var x = document.getElementById('qrcode');
 
-    if (x.style.display === 'none') {
+    if (x.style.display == 'none') {
       x.style.display = 'block';
     } else {
       x.style.display = 'none';
@@ -24,32 +42,42 @@ function Wifi() {
   }
 
   return (
-    <Container>
-      <Row>
-        <Col md="3">
-          <h2>Wifi Password:</h2>
-        </Col>
-        <Col md="2">
-          <p>112233445566</p>
-        </Col>
-        <Col md="7">
+    <GiveMargin>
+      <Container>
+        <Row>
+          <Col md="12">
+            <InfoHeader>
+              <h1>Password do Wifi</h1>
+              <img src={wifi} alt="icone de wifi" />
+            </InfoHeader>
+          </Col>
+          <Col md="12">
+            <Cartao>
+              <h1>Wifi Password:</h1>
+              <span>112233445566</span>
+            </Cartao>
+          </Col>
           <Hidding>
-            <img
-              id="qrcode"
-              className="hidding"
-              src={qrcode}
-              alt="password e rede de internet"
-            />
+            <Col md="12" align="center">
+              <img
+                id="qrcode"
+                className="hidding"
+                src={qrcode}
+                alt="password e rede de internet"
+              />
+            </Col>
+            <Col md="12">
+              <Button className="btn btn-primary" onClick={handleClick}>
+                Gerar QR Code
+              </Button>
+            </Col>
           </Hidding>
-        </Col>
-      </Row>
+        </Row>
 
-      <br />
-      <br />
-      <Button className="btn btn-primary" onClick={handleClick}>
-        Gerar QR Code
-      </Button>
-    </Container>
+        <br />
+        <br />
+      </Container>
+    </GiveMargin>
   );
 }
 export default Wifi;
--- src/style.js
+++ src/style.js
@@ -24,6 +24,10 @@ color:#282828;
     display:flex-end;
   }
 `;
+export const GiveMargin = style.div`
+height:100%;
+margin-bottom:120px;
+`;
 
 export const Header = style.div`
 width:100%;
@@ -44,7 +48,29 @@ justify-content:center;
 text-align:center;
 color:#B3B2B2;
 border-radius:15px 15px 0 0;
+margin-top:120px;
 img{
   height:70px;
+}
+`;
+
+export const InfoHeader = style.div`
+width:100%;
+background-color:white;
+display:flex;
+align-items:center;
+justify-content:space-between;
+border-radius:50px;
+margin-bottom:30px;
+height:100px;
+h1{
+  font-size:35px;
+  margin-left:5%;
+}
+
+img{
+  height:50px;
+  float:right;
+  margin-right:5%;
 }
 `;
