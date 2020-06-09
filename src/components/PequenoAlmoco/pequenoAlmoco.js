45c2311cdbafb1d13b4741a231fd5f58f0fdf97a
trox115 <tofernandes67@gmail.com>
2020-06-09 09:24:00 +0100

    pequeno almoço done

--- src/components/PequenoAlmoco/pequenoAlmoco.js
+++ src/components/PequenoAlmoco/pequenoAlmoco.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
@@ -7,6 +7,10 @@ import Button from 'react-bootstrap/Button';
 import styled from 'styled-components';
 import { InfoHeader, GiveMargin } from '../../style';
 import Pequeno from '../../assets/img/6.png';
+import setHours from 'date-fns/setHours';
+import setMinutes from 'date-fns/setMinutes';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const InfoHeader2 = styled.div`
   width: 100%;
@@ -46,8 +50,71 @@ export const Cartao2 = styled.div`
   }
 `;
 
+const InfoHeader3 = styled.div`
+  width: 100%;
+  background-color: white;
+  text-align: center;
+  align-items: center;
+  border-radius: 50px;
+  text-align: center;
+  height: 80px;
+  input {
+    margin-top: 5%;
+    margin-left: 20px;
+    margin-right: 20px;
+    font-size: 25px;
+    border: none;
+    border-bottom: 1px solid pink;
+  }
+`;
+
+export const Cartao3 = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  background-color: white;
+  border-radius: 30px;
+  margin-bottom: 5%;
+  color: #282828;
+  h1 {
+    font-size: 20px;
+    width: 60%;
+    margin-left: 2%;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 700;
+  }
+
+  img {
+    width: 55px;
+    height: 55px;
+    display: flex-end;
+  }
+`;
+
+export const Compra = styled.button`
+  font-size: 25px;
+  background-color: #e84865;
+  color: white;
+  border: none;
+  height: 50px;
+  margin-left: 10px;
+  border-radius: 10px;
+  padding: 0 20px 0 20px;
+  align-itens: center;
+  justify-content: center;
+`;
+
 function PequenoAlmoco({ ...props }) {
   const { user } = props;
+  const [startDate, setStartDate] = useState(
+    setHours(setMinutes(new Date(), 30), 8),
+  );
+
+  function handleClick() {
+    console.log(startDate);
+    console.log(user[0].id);
+  }
+
   return (
     <GiveMargin>
       <Container>
@@ -76,12 +143,29 @@ function PequenoAlmoco({ ...props }) {
               </InfoHeader2>
             </Cartao2>
 
-            <Cartao2>
-              <InfoHeader2>
-                <input type="date" />
-                <input type="time" />
-              </InfoHeader2>
-            </Cartao2>
+            <Cartao3>
+              <InfoHeader3>
+                <DatePicker
+                  selected={startDate}
+                  onChange={date => setStartDate(date)}
+                  locale="pt-PT"
+                  showTimeSelect
+                  timeFormat="p"
+                  timeIntervals={30}
+                  dateFormat="Pp"
+                  includeTimes={[
+                    setHours(setMinutes(new Date(), 0), 8),
+                    setHours(setMinutes(new Date(), 30), 8),
+                    setHours(setMinutes(new Date(), 0), 9),
+                    setHours(setMinutes(new Date(), 30), 9),
+                    setHours(setMinutes(new Date(), 0), 10),
+                    setHours(setMinutes(new Date(), 30), 10),
+                  ]}
+                  dateFor
+                />
+                <Compra onClick={handleClick}> Submeter </Compra>
+              </InfoHeader3>
+            </Cartao3>
           </Col>
         </Row>
 
